7f677a884ca61e6aaa8cfbdee1ff896a42697fae
Skyknight47 <42124564+Skyknight47@users.noreply.github.com>
2019-08-07 04:34:40 +0200

    Update code.js

--- code.js
+++ code.js
@@ -43,7 +43,7 @@ bot.on('message', async message => {
   const verifiedRole = message.guild.roles.find(role => role.name === "Verified");
   const verificationCode = ['apple', 'rain', 'dog', 'cat', 'food','yum','pizza','raindrop','snow','birthday','cake','burger','soda','ice','no','yes','orange','pear','plum'];
   const promoLogs = bot.channels.get(`${xpAuditLogChannelID}`)
-  const officerRole = message.guild.roles.find(role => role.name === `${officerRoleE}`);
+  const officerRole = message.guild.roles.find(role => role.name === `${config.officerRoleE}`);
   const groupFunction = await bloxyClient.getGroup(groupID)
 
 
@@ -117,8 +117,8 @@ bot.on('message', async message => {
   }
 
   if (message.content.toLowerCase().startsWith(`${prefix}xp`)){
-    if (!message.member.roles.exists("name", `${officerRole}`)){
-      return message.channel.send(`Sorry ${message.author}, but only users with the **\`${officerRoleE}\`** can run that command!`).then(message => message.delete(5000));
+   if (!message.member.roles.exists("name", `${config.officerRoleE}`)){
+      return message.channel.send(`Sorry ${message.author}, but only users with the **\`${config.officerRoleE}\`** can run that command!`).then(message => message.delete(5000));
     }
     if (!args[1]){
       return message.channel.send(`Sorry ${message.author}, but you're missing the first argument--add or remove?\n**Adding XP: \`${prefix}xp add 1 username1, username2, username3...\`\nRemoving XP: \`${prefix}xp remove 1 username1, username2, username3...\`**`).then(message => message.delete(5000));
